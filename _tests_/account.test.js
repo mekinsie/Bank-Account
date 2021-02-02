b521df9366ff80453458932aa210e20b015ec905
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-02 11:57:11 -0800

    Create and successfully fail third test.
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- _tests_/account.test.js
+++ _tests_/account.test.js
@@ -1,9 +1,25 @@
 import Account from './../src/account.js'
 
+
 describe('Account', () => {
+  let reusableAccount;
+
+  beforeEach(() => {
+  reusableAccount = new Account(); 
+  });
   // All Tests go in here //
   test('should correctly create an account object with a current balance property with value 0', () => {
-    const account = new Account();
-    expect(account.currentBalance).toEqual(0);
+    // const reusableAccount = new Account(); 
+    expect(reusableAccount.currentBalance).toEqual(0);
+  });
+
+  test('should take in value from user and add to currentBalance', () => {
+    let inputValue = 4;
+    expect(reusableAccount.deposit(inputValue)).toEqual(4);
+  }); 
+
+  test('should take in value from user and subtract to currentBalance', () => {
+    let inputValue = 4;
+    expect(reusableAccount.withdrawal(inputValue)).toEqual(-4);
   });
 });
